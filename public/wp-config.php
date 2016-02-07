06a0cf4696ac2d45a16a0be837b10da4b48acd1a
rickisen <rickard.spam@gmail.com>
2016-02-07 20:41:38 +0100

    now uses external db

--- public/wp-config.php
+++ public/wp-config.php
@@ -19,23 +19,26 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+
+$config = parse_ini_file('../Assets/mysqliConfig.ini');
+
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', $config['database']);
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', $config['user']);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', $config['password']);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $config['host']);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 'utf8_bin');
 
 /**#@+
  * Authentication Unique Keys and Salts.
